40f3bc5fd38b532fe02b1720f9669a2a3b9c51c9
JacquesX <jacques.xuereb.23@um.edu.mt>
2025-02-25 13:11:10 +0100

    Removed Stemmer

--- stemmer/using_stemmer.py
+++
@@ -1,7 +0,0 @@
-import stemmer as st
-
-sentence = ["Bonġu", "kif", "int", "jien", "jisimni", "Jacques", "u", "qiegħed", "nivverifika", "l-użu", "tal-lingwa", "Maltija", "f'dan", "il-programm"]
-returned = []
-
-for i in sentence:
-    print(str(i) + " -- " + st.stemm(i))
